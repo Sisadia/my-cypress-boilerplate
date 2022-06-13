61fc30be922198ccd694fdb0b8f4ae42030b4429
SMShoron <shoronsrs@gmail.com>
2022-06-13 13:44:10 +0600

    Add: Initial File

---
+++ cypress/e2e/cypresstemp.cy.js
@@ -0,0 +1,51 @@
+/// <reference types="Cypress"/>
+
+describe('First Suite', ()=>{
+
+    describe('Suite Section',() =>{
+
+        beforeEach('code for every test', ()=>{
+
+            //repetitive code like login functionality
+        })
+
+        it('Test Name', () =>{
+
+        })
+
+        it('Test Name', () =>{
+
+        })
+
+        it('Test Name', () =>{
+
+        })
+    })
+
+    it('First test', () =>{
+
+    })
+
+    it('Second test', () =>{
+        
+    })
+
+    it('Third test', () =>{
+        
+    })
+})
+
+describe('Second Suite', ()=>{
+
+    it('First test', () =>{
+
+    })
+
+    it('Second test', () =>{
+        
+    })
+
+    it('Third test', () =>{
+        
+    })
+})
